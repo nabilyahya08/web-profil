2133a0f2ee8280c498050fc7f92dd7070e17f384
yanuarfa <yanuarfatur41@gmail.com>
2024-06-10 17:47:31 +0700

    first commit

---
+++ resources/views/frontend/content/kalenderakademik.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.frontend.app')
+@section('title')
+    Kalender Akademik
+@endsection
+
+@section('content')
+@section('about')
+    <div class="container">
+        <h1>Kalender Akademik</h1>
+    </div>
+@endsection
+@endsection
